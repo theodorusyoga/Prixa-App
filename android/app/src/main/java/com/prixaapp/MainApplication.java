0b81cc29f3f8fbeb468a89f78273868284bc7c02
Theodorus Yoga Mahendraputra <theoyoga@outlook.com>
2020-02-01 15:55:18 +0700

    Add voximplant and wheel picker android

--- android/app/src/main/java/com/prixaapp/MainApplication.java
+++ android/app/src/main/java/com/prixaapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.prixaapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.wheelpicker.WheelPickerPackage;
+import com.voximplant.reactnative.VoxImplantReactPackage;
 import com.zxcpoiu.incallmanager.InCallManagerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.proyecto26.inappbrowser.RNInAppBrowserPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new WheelPickerPackage(),
+            new VoxImplantReactPackage(),
             new InCallManagerPackage(),
             new RNGestureHandlerPackage(),
             new RNInAppBrowserPackage(),
